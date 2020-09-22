ba45cd19f37e37fe4f74a113592f047090a78329
sithu <a.sithu@gmail.com>
2020-09-21 21:52:45 -0700

    HUGO_ENV=production

---
+++ hugo/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+set HUGO_ENV=production
+
+# Build the project with ananke theme.
+hugo -D
+
+# Copy published docs to root level where Github pages read at https://getcandle.github.io/.
+cp -rf public/* ..
+
+# delete the published folder
+# rm -rf docs
+
+# Add changes from hugo folder and the root folder to git.
+git add ..
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
